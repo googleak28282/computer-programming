4b8e37636ff2c98b7e10dc16d67dfbd935d20980
googleak28282 <410689@gm.tnfsh.tn.edu.tw>
2019-10-17 13:35:49 +0800

    add ex02_2

---
+++ ex02/ex02_2.py
@@ -0,0 +1,9 @@
+s=input("name: ")
+if s=="Neo":
+	print("Hello, Neo. You are the one!")
+else:
+	n=int(input("age: "))
+	if n>=18:
+		print("Do you want to join my army?")
+	else:
+		print("You are not an adult. See you later.")
